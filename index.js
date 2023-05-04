41b9d053717b7715b3bb3e2cb4d8c468629ba9ec
eshansaif <35582062+eshansaif@users.noreply.github.com>
2023-05-04 15:46:45 +0600

    cors middleware used

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
 const app = express()
 const port = 5000
+app.use(cors())
+
 
 const chefData = require('./data/chef.json')
 
